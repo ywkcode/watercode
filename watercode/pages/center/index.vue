0c3faaddf2b85ce102936c51608dbdffc65f21fc
yinweikang <yinweikang@c2matica.com>
2022-03-02 21:28:12 +0800

    test

--- watercode/pages/center/index.vue
+++ watercode/pages/center/index.vue
@@ -1,14 +1,6 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">
-				uView - 多平台快速开发的UI框架
-			</text>
-		</view>
-		<view class="link-demo">
-			<u-link :color="$u.color['primary']" :under-line="true" href="http://www.uviewui.com">Link超链接组件演示</u-link>
-		</view>
+		 
 	</view>
 </template>
 
