28e16aae808b92f37a6237f133a85d302d4385d9
yinweikang <yinweikang@c2matica.com>
2022-03-16 23:53:12 +0800

    小程序首页

--- watercode/pages/index/index.vue
+++ watercode/pages/index/index.vue
@@ -1,32 +1,85 @@
 <template>
-	<view class="content">
+	<view>
+		<view class="wrap">
+			<u-swiper :list="list" height="320"></u-swiper>
+		</view>
+	     <view class="u-padding-20 mytitle">数据指标</view>
+	      <u-line color="#bbb"></u-line>
+		  <!-- <view>
+		 <u-icon name="photo" color="#2979ff" size="128"
+		  label="水质" labelPos="bottom" space="3px" labelColor="gray"></u-icon>
+		  <p style="display:inline-block;">1111</p>
+		 </view> -->
+		 <navigator url="/pages/center/index" open-type="switchTab" hover-class="other-navigator-hover">
+		 				 <view style="margin: 20px;height: 60px;width:100%;position: relative;" >
+		 				 			  <view style="display: inline-block; width:60px;height: 60px;background-image: url(../../static/water.png); background-size: cover;"></view>
+		 				 			  <view style="display: inline-block;width:80%;height: 60px;float:right;text-overflow: clip;">
+		 				 				  <p style="font-weight: bolder">水质监测</p>
+		 				 				  <p style="text-overflow: clip;">TOC,AD,ZL,LL</p>
+		 				 				  <span style="position: absolute;right: 24px;bottom: 20px;"><u-icon name="arrow-right" color="#2979ff" size="28"></u-icon></span>
+		 				 			  </view>
+		 				 </view>
+		 				</navigator>
 		
-		  <view class="u-padding-30">统计图原型</view>
-		  <gauge></gauge>
-		  
-		  
+		  <navigator url="/pages/center/index" open-type="switchTab" hover-class="other-navigator-hover">
+		      <view style="margin: 20px;height: 60px;width:100%;position: relative;" >
 		
+		 			  <view style="display: inline-block; width:60px;height: 60px;background-image: url(../../static/gas.png); background-size: cover;"></view>
+		 			  <view style="display: inline-block;width:80%;height: 60px;float:right;text-overflow: clip;">
+		 				  <p style="font-weight: bolder">气体监测</p>
+		 				  <p style="text-overflow: clip;">HCL,氨气,CO2,HCL</p>
+						    <span style="position: absolute;right: 24px;bottom: 20px;"><u-icon name="arrow-right" color="#f56c6c" size="28"></u-icon></span>
+		 			  </view>
+					  
+		 </view>
+		 </navigator>
 	</view>
+	
+
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
-				title: 'Hello',
-				 
+					 
+				list: [{
+						image: 'https://cdn.uviewui.com/uview/swiper/1.jpg',
+						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
+					},
+					{
+						image: 'https://cdn.uviewui.com/uview/swiper/2.jpg',
+						title: '身无彩凤双飞翼，心有灵犀一点通'
+					},
+					{
+						image: 'https://cdn.uviewui.com/uview/swiper/3.jpg',
+						title: '谁念西风独自凉，萧萧黄叶闭疏窗，沉思往事立残阳'
+					}
+				],
+				  
+				 title:"行业咨询"
 			}
 		},
-		onLoad() {
-
+		async onLoad() { 
+			  
 		},
 		methods: {
-
+            click(name) {
+                           this.$refs.uToast.success(`点击了第${name}个`)
+                       } 
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
+	.grid-text {
+	        font-size: 14px;
+	        color: #909399;
+	        padding: 10rpx 0 20rpx 0rpx;
+	        /* #ifndef APP-PLUS */
+	        box-sizing: border-box;
+	        /* #endif */
+	    }
 	.content {
 		display: flex;
 		flex-direction: column;
@@ -48,17 +101,37 @@
 		display: flex;
 		justify-content: center;
 	}
-	
+
 	.title {
 		font-size: 28rpx;
 		color: $u-content-color;
 	}
-	
+
 	.button-demo {
 		margin-top: 80rpx;
 	}
-	
+
 	.link-demo {
 		margin-top: 80rpx;
 	}
+	.mytitle{
+		text-align: left;
+		font-size:18px;
+	    margin: 10px 5px;
+	}
+	.myul{
+		list-style-type: none;
+	}
+	.mystylediv
+	{
+		width:60px;
+		height: 60px;
+		
+	}
+	.mystylediv img{
+		width:100% !important;
+		height: 100% !important;
+		background-size: 0px;
+	}
+	 
 </style>
